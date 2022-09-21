9554affd632accf4f68344eb452a885567d24acf
Juan <juan.nascimento.999@gmail.com>
2022-09-21 14:05:08 -0300

    aula 153-157

---
+++ CursoNelioAlvesJava/src/Heranca.java
@@ -0,0 +1,13 @@
+package Package;
+
+import Entities2.BusinessAccount;
+
+public class Heranca {
+
+	public static void main (String[]args) {
+		
+		BusinessAccount account = new BusinessAccount();
+		
+		account.getBalance();
+	}
+}
